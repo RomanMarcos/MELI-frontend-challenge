5d4f8b47d76861e068ad897c822de2ac30840e16
marcos <marcoss.-08@hotmail.com>
2024-03-06 18:42:30 -0300

    fix for warning 'No routes matched location /'

--- frontend-application/src/Router.js
+++ frontend-application/src/Router.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom';
 import { Results } from './views/results/Results';
 import { ProductDetail } from './views/productDetail/ProductDetail';
+import { Home } from './views/home/Home';
 
 export const Router = () => {
   return (
     <Routes>
+        <Route path='/' element={<Home />} />
         <Route path='/items' element={<Results />} />
         <Route path='/items/:id' element={<ProductDetail />} />
     </Routes>
